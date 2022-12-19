77b6aa7a7f6b9225e7c8d377b0c9b82fdccca725
FranKBrownn <99732193+FranKBrownn@users.noreply.github.com>
2022-12-19 22:33:26 +0000

    x

--- manage.py
+++ manage.py
@@ -2,29 +2,6 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
-from flask import Flask, render_template
-
-app = Flask(__name__)
-
-
-@app.route("/")
-def index():
-    return render_template("index.html")
-
-
-@app.route("/booking")
-def booking():
-    return render_template("booking.html")
-
-
-@app.route("/gallery")
-def gallery():
-    return render_template("gallery.html")
-
-
-@app.route("/menu")
-def menu():
-    return render_template("menu.html")
 
 if __name__ == "__main__":
     app.run(
