1a8ca4aaab476d899d5cb6416b14f8f81c9f2096
FranKBrownn <99732193+FranKBrownn@users.noreply.github.com>
2022-12-19 21:16:20 +0000

    swaped mz code from mz run.py file to my manage.py file

--- manage.py
+++ manage.py
@@ -2,6 +2,36 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+
+@app.route("/booking")
+def booking():
+    return render_template("booking.html")
+
+
+@app.route("/gallery")
+def gallery():
+    return render_template("gallery.html")
+
+
+@app.route("/menu")
+def menu():
+    return render_template("menu.html")
+
+if __name__ == "__main__":
+    app.run(
+        host=os.environ.get("ip", "0.0.0.0"),
+        port=int(os.environ.get("PORT", "5000")),
+        debug=True
+    )
 
 
 def main():
