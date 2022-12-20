a328192f46ff9ce69914b5233aa986e408f0422d
FranKBrownn <Frankcdbrown97@gmail.com>
2022-12-20 18:06:05 +0000

    created my database

--- booking/models.py
+++ booking/models.py
@@ -1,3 +1,16 @@
 from django.db import models
+from django.contrib.auth.models import User
+from cloudinary.models import CloudinaryField
 
-# Create your models here.
+
+class booking(models.Model):
+    fname = models.CharField(max_length=20)
+    lname = models.CharField(max_length=20)
+    email = models.EmailField()
+    phone = models.IntegerField()
+    date = models.DateField()
+    time = models.TimeField()
+
+
+def __STR__(self):
+    return self.name
--- booking/views.py
+++ booking/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from .models import booking
+
+from booking.models import booking
+
+def book_a_table(request):
+    
 
 # Create your views here.
